d580d42fb335d3d721c378ca271a16eb51d1ffbc
vash512 <phyrox.vash512@gmail.com>
2015-01-03 15:34:37 -0600

    areglado el error url summernote

--- back-end/phyro/phyro/urls.py
+++ back-end/phyro/phyro/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^summernote/', include('django_summernote.urls')),
     #url(r'^',include('home.urls')),
 )
