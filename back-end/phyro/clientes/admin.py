59a0f56f720ddc7a8a7aeffef2f5ea4f00a98347
vash512 <phyrox.vash512@gmail.com>
2015-01-03 15:27:01 -0600

    configuracion summer note en clientes

--- back-end/phyro/clientes/admin.py
+++ back-end/phyro/clientes/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from clientes.models import Cliente, Proyecto, Etapa, Requerimiento, Tarea, Preferencia, RequerimientoUsuario
 
+class ProyectoAdmin(SummernoteModelAdmin):
+    model = Proyecto
+
 admin.site.register(Cliente)
-admin.site.register(Proyecto)
+admin.site.register(Proyecto, ProyectoAdmin)
 admin.site.register(Etapa)
 admin.site.register(Requerimiento)
 admin.site.register(Tarea)
